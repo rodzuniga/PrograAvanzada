e1043727bd3d98536c2f6138e276021038969230
soportetecnicolatina <soportetecnicolatina@outlook.es>
2017-05-29 20:25:31 -0600

    Se agrega proyecto logicqa de ngocio

---
+++ Ulatina.PrograAvanzada-BLAplicacionWeb/Ulatina.PrograAvanzada-BLAplicacionWeb/Dominio/Acciones/ObtenerFechaActual.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ulatina.PrograAvanzada_BLAplicacionWeb.Dominio.Acciones
+{
+    public class ObtenerFechaActual
+    {
+        public string ObtengaLaFecha()
+        {
+            // Especificaciones.ObtengaFecha laEspecificacion = new
+            //   Especificaciones.ObtengaFecha();
+
+            var laEspecificacion = new
+                Especificaciones.ObtengaFecha();
+            var laFecha = laEspecificacion.FormatearUnaFecha(DateTime.Now,"dd/mm/yyyy");
+            return laFecha;
+
+        }
+    }
+}
---
+++ Ulatina.PrograAvanzada-BLAplicacionWeb/Ulatina.PrograAvanzada-BLAplicacionWeb/Dominio/Especificaciones/ObtengaFecha.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ulatina.PrograAvanzada_BLAplicacionWeb.Dominio.Especificaciones
+{
+    public class ObtengaFecha
+    {
+    public string  FormatearUnaFecha(DateTime laFecha, string elFormato)
+        {
+            return "Hello World";
+        }
+    }
+}
